a1284a0f6855e43c4c1cb246e1db515d3bda2625
ttingchia <t.c.chiang@tudelft.nl>
2021-06-29 16:09:11 +0200

    Remove one redundant line of code and a trailing whitspace

--- src/vehicles/RosVehicle.lua
+++ src/vehicles/RosVehicle.lua
@@ -225,10 +225,9 @@ function RosVehicle:getLaserFrameNode()
         local current_veh_name = ros.Names.sanatize(self:getFullName())
         spec.laser_scan_obj = LaserScanner.new(self, mod_config.vehicle[current_veh_name])
 
-        local spec = self.spec_rosVehicle
         local instance_veh = VehicleCamera:new(self, RosVehicle)
         local xml_path = instance_veh.vehicle.configFileName
-        local xmlFile = loadXMLFile("vehicle", xml_path) 
+        local xmlFile = loadXMLFile("vehicle", xml_path)
         --  get the cameraRaycast node 2(on top of ) which is 0 index .raycastNode(0)
         --  get the cameraRaycast node 3 (in the rear) which is 1 index .raycastNode(1)
         local cameraKey = string.format("vehicle.enterable.cameras.camera(%d).%s", 0, spec.laser_scan_obj.vehicle_table.laser_scan.laser_attachments)
