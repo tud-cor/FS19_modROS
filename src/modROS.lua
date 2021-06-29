d316227d0bf4bb44d3c408ae98fe33dbc8447a62
ttingchia <t.c.chiang@tudelft.nl>
2021-06-29 16:05:02 +0200

    Delete the part where RosVehicle spec is dealing with

--- src/modROS.lua
+++ src/modROS.lua
@@ -308,44 +308,6 @@ function ModROS:rosPubMsg(flag)
                 return
             end
         end
-
-        -- raycastNode initialization
-        local vehicle = g_currentMission.controlledVehicle
-        -- if the player is not in the vehicle, print error and return
-        if not vehicle then
-            print("You are not inside any vehicle, come on! Enter 'e' to hop in one next to you!")
-            return
-        else
-            self.instance_veh = VehicleCamera:new(vehicle, ModROS)
-            local xml_path = self.instance_veh.vehicle.configFileName
-            local xmlFile = loadXMLFile("vehicle", xml_path)
-            -- index 0 is outdoor camera; index 1 is indoor camera
-            -- local cameraKey = string.format("vehicle.enterable.cameras.camera(%d)", 0)
-
-            --  get the cameraRaycast node 2(on top of ) which is 0 index .raycastNode(0)
-            --  get the cameraRaycast node 3 (in the rear) which is 1 index .raycastNode(1)
-
-            local cameraKey = string.format("vehicle.enterable.cameras.camera(%d).raycastNode(0)", 0)
-            XMLUtil.checkDeprecatedXMLElements(xmlFile, xml_path, cameraKey .. "#index", "#node") -- FS17 to FS19
-            local camIndexStr = getXMLString(xmlFile, cameraKey .. "#node")
-            self.instance_veh.cameraNode =
-                I3DUtil.indexToObject(
-                self.instance_veh.vehicle.components,
-                camIndexStr,
-                self.instance_veh.vehicle.i3dMappings
-            )
-            if self.instance_veh.cameraNode == nil then
-                print("nil camera")
-            -- else
-            --     print(instance_veh.cameraNode)
-            end
-            -- create self.laser_frame_1 attached to raycastNode (x left, y up, z into the page)
-            -- and apply a transform to the self.laser_frame_1
-            local tran_x, tran_y, tran_z = mod_config.laser_scan.laser_transform.translation.x, mod_config.laser_scan.laser_transform.translation.y, mod_config.laser_scan.laser_transform.translation.z
-            local rot_x, rot_y, rot_z = mod_config.laser_scan.laser_transform.rotation.x, mod_config.laser_scan.laser_transform.rotation.y, mod_config.laser_scan.laser_transform.rotation.z
-            self.laser_frame_1 = frames.create_attached_node(self.instance_veh.cameraNode, "self.laser_frame_1", tran_x, tran_y, tran_z, rot_x, rot_y, rot_z)
-        end
-
         -- initialisation was successful
         self.doPubMsg = true
 
