22711a23f7cde89594d0d58b5e4fee344f6e3756
ttingchia <t.c.chiang@tudelft.nl>
2021-06-29 16:07:26 +0200

    Make each laser sensor configurable

--- src/mod_config.lua
+++ src/mod_config.lua
@@ -19,37 +19,136 @@ author: G.A. vd. Hoorn
 
 mod_config =
 {
-  controlled_vehicle = {
-    base_link_frame_id = "case_ih_7210_pro_9"
-  },
-  laser_scan = {
-    num_rays = 128,
-    num_layers = 5,
-    angle_min = -math.pi,
-    angle_max = math.pi,
-    range_min = 0.1,
-    range_max = 30,
-    ignore_terrain = true,
-    inter_layer_distance = 0.1,
-    -- apply a transform to first laser scan frame
-    laser_transform = {
-      translation = {
-          x = 0.0,
-          y = 0.0,
-          z = 0.0
+  control_only_active_one = false,
+  vehicle = {
+    case_ih_7210_pro = {
+      laser_scan = {
+        num_rays = 64,
+        num_layers = 2,
+        angle_min = -math.pi,
+        angle_max = math.pi,
+        range_min = 0.1,
+        range_max = 30,
+        ignore_terrain = true,
+        inter_layer_distance = 0.1,
+        -- apply a transform to first laser scan frame
+        laser_transform = {
+          translation = {
+              x = 0.0,
+              y = -1.5,
+              z = 0.0
+          },
+          rotation = {
+              -- positive rotation over X rotates laser scanner down (ie: math.pi/6)
+              x = 0.0,
+              y = 0.0,
+              z = 0.0
+          }
+        },
+        laser_attachments = "raycastNode(0)"
       },
-      rotation = {
-          -- positive rotation over X rotates laser scanner down (ie: math.pi/6)
-          x = 0.0,
-          y = 0.0,
-          z = 0.0
+      -- the collision_mask for raycasting can be customized. Both
+      -- decimal and hexadecimal notations are supported (ie: 9 and 0x9).
+      raycast = {
+        collision_mask = nil
+      }
+    },
+    diesel_locomotive = {
+      laser_scan = {
+        num_rays = 64,
+        num_layers = 2,
+        angle_min = -math.pi,
+        angle_max = math.pi,
+        range_min = 0.1,
+        range_max = 30,
+        ignore_terrain = true,
+        inter_layer_distance = 0.1,
+        -- apply a transform to first laser scan frame
+        laser_transform = {
+          translation = {
+              x = 0.0,
+              y = -1.5,
+              z = 0.0
+          },
+          rotation = {
+              -- positive rotation over X rotates laser scanner down (ie: math.pi/6)
+              x = 0.0,
+              y = 0.0,
+              z = 0.0
+          }
+        },
+        laser_attachments = "raycastNode(0)"
+      },
+      -- the collision_mask for raycasting can be customized. Both
+      -- decimal and hexadecimal notations are supported (ie: 9 and 0x9).
+      raycast = {
+        collision_mask = nil
+      }
+    },
+    fiat_1300dt = {
+      laser_scan = {
+        num_rays = 64,
+        num_layers = 2,
+        angle_min = -math.pi,
+        angle_max = math.pi,
+        range_min = 0.1,
+        range_max = 30,
+        ignore_terrain = true,
+        inter_layer_distance = 0.1,
+        -- apply a transform to first laser scan frame
+        laser_transform = {
+          translation = {
+              x = 0.0,
+              y = -1.5,
+              z = 0.0
+          },
+          rotation = {
+              -- positive rotation over X rotates laser scanner down (ie: math.pi/6)
+              x = 0.0,
+              y = 0.0,
+              z = 0.0
+          }
+        },
+        laser_attachments = "raycastNode(0)"
+      },
+      -- the collision_mask for raycasting can be customized. Both
+      -- decimal and hexadecimal notations are supported (ie: 9 and 0x9).
+      raycast = {
+        collision_mask = nil
+      }
+    },
+    new_holland_tx_32 = {
+      laser_scan = {
+        num_rays = 64,
+        num_layers = 2,
+        angle_min = -math.pi,
+        angle_max = math.pi,
+        range_min = 0.1,
+        range_max = 30,
+        ignore_terrain = true,
+        inter_layer_distance = 0.1,
+        -- apply a transform to first laser scan frame
+        laser_transform = {
+          translation = {
+              x = 0.0,
+              y = -3,
+              z = 0.0
+          },
+          rotation = {
+              -- positive rotation over X rotates laser scanner down (ie: math.pi/6)
+              x = 0.0,
+              y = 0.0,
+              z = 0.0
+          }
+        },
+        laser_attachments = "raycastNode(0)"
+      },
+      -- the collision_mask for raycasting can be customized. Both
+      -- decimal and hexadecimal notations are supported (ie: 9 and 0x9).
+      raycast = {
+        collision_mask = nil
       }
     }
-  },
-  -- the collision_mask for raycasting can be customized. Both
-  -- decimal and hexadecimal notations are supported (ie: 9 and 0x9).
-  raycast = {
-    collision_mask = nil
   }
 }
 
